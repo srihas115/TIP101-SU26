38489979e5502ece7b4408439129a192a07b94f0
srihas <srihasgupta@gmail.com>
2026-07-09 19:55:23 -0500

    Finished Unit 06 Session 2 PSET 1 Problems 1-2. Started Problem 3
    Co-authored-by: Jessica Keodara <jessicakeodara@users.noreply.github.com>
    Co-authored-by: Saratou Bako Bagassa <bakobagassas@users.noreply.github.com>

--- Unit 06/Session 2/Problem Set 1/Problem 3: Partition List Around Value.py
+++ Unit 06/Session 2/Problem Set 1/Problem 3: Partition List Around Value.py
@@ -3,8 +3,35 @@ class Node:
         self.value = value
         self.next = next
 
+'''
+1. understand
+
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+result:
+1 -> 2 -> 2 -> 4 -> 3 -> 5 or 2 -> 2 -> 1 -> 5 -> 4 -> 3
+
+
+create two linked lists: greater or less than the value
+
+'''
+
 def partition(head, val):
+    smaller = None
+    bigger = None
+    curr = 
+    
+    
     pass
 
 # 1 -> 4 -> 3 -> 2 -> 5 -> 2
+# small list = 1 -> 2 -> 2
+# big list = 4 -> 3 -> 5
+
+
+# result: 1 -> 2 -> 2 -> 4 -> 3 -> 5 
+
 # head = 1, val = 3
+
+# i really want to work on this later - jessica
