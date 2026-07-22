81b50172c15f023ba6c5953506ab858461968f78
srihas <srihasgupta@gmail.com>
2026-07-22 17:32:07 -0500

    Fixed Unit 01 Session 1 Version 2 Problem 7

--- Unit 01 - Python & Lists/Session 1/Version 2/07_halve_lst.py
+++ Unit 01 - Python & Lists/Session 1/Version 2/07_halve_lst.py
@@ -32,9 +32,9 @@ Plan:
 def halve_lst(lst):
     result = []
     for number in lst:
-        halved = number/2
+        halved = number / 2
         # result.append(halved)
-        result.append(int(halved)) # should be int(halved))
+        result.append(halved) # should be int(halved))
     return result
 
 print(halve_lst([2,4,6,8]))
